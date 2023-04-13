42fddc0a63fcb4eedcdbb078050556c80aade513
Zizhan Tang <mtang410@usc.edu>
2023-04-13 01:22:31 +0000

    updated

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,10 +20,102 @@ using namespace std;
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
-// Add prototypes for any helper functions here
+bool can_assign_worker(
+    const vector<size_t>& work,
+    const AvailabilityMatrix& avail,
+    const size_t maxShifts,
+    const size_t row,
+    const size_t worker_id
+)
+{
+   if(avail[row][worker_id] == 1 && work[worker_id] < maxShifts) return true;
+   else return false;
+}
 
+bool assign_worker(
+    DailySchedule& sched,
+    vector<size_t>& work,
+    const size_t row,
+    const size_t col,
+    const size_t worker_id
+)
+{
+    sched[row][col] = worker_id;
+    work[worker_id]++;
+    return true;
+}
 
-// Add your implementation of schedule() and other helper functions here
+bool unassign_worker(
+    DailySchedule& sched,
+    vector<size_t>& work,
+    const size_t row,
+    const size_t col,
+    const size_t worker_id
+)
+{
+    sched[row][col] = sched.size();
+    work[worker_id]--;
+    return false;
+}
+
+bool is_schedule_complete(
+    const size_t dailyNeed,
+    const size_t col
+)
+{
+    if(col == dailyNeed) return true;
+    else return false;
+}
+
+bool is_day_complete(
+    const AvailabilityMatrix& avail,
+    const size_t row
+)
+{
+    if(row == avail.size()) return true;
+    else return false;
+}
+
+bool is_worker_valid(
+    const vector<Worker_T>& temp,
+    const Worker_T curr,
+    const size_t col
+)
+{
+    if(col == 0 || (find(temp.begin(), temp.end(), curr) != temp.end())) return true;
+    else return false;
+}
+
+bool helperFunction(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& work,
+    size_t row,
+    size_t col
+)
+{
+    if (is_schedule_complete(dailyNeed, col)) {
+        col = 0;
+        row++;
+    }
+   
+    if (is_day_complete(avail, row)) {
+        return true;
+    }
+
+    for(Worker_T i = 0; i < avail[row].size(); i++) {
+        if (is_worker_valid(sched[row], i, col) && can_assign_worker(work, avail, maxShifts, row, i)) {
+            assign_worker(sched, work, row, col, i);
+            if (helperFunction(avail, dailyNeed, maxShifts, sched, work, row, col + 1)) {
+                return true;
+            }
+            unassign_worker(sched, work, row, col, i);
+        }
+    }
+    return false;
+}
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -32,14 +124,17 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
-        return false;
+    if(avail.size() == 0) return false;
+    else{
+        sched.clear();
+
+        for (size_t i = 0; i < avail.size(); i++) {
+            vector<Worker_T> temp(dailyNeed, avail[0].size());
+            sched.push_back(temp);
+        }
+
+        vector<size_t> work(avail[0].size(), 0);
+        return helperFunction(avail, dailyNeed, maxShifts, sched, work, 0, 0);
     }
-    sched.clear();
-    // Add your code below
-
-
-
-
+    
 }
-
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+bool isWordValid(const std::string& word, const std::set<std::string>& dict);
+void getWords(const std::string& input, const std::string& floating, size_t index,std::string current,std::set<std::string>& words);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,57 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> words;
+		set<string> ValidWords;
+    size_t index = count(in.begin(), in.end(), '-');
+    getWords(in, floating, index, "", words);
+		
+		for (auto iter = words.begin(); iter != words.end(); ++iter) {
+        if (isWordValid(*iter, dict)) {
+          ValidWords.insert(*iter);
+        }
+    }
+    return ValidWords;
+}
 
+bool isWordValid(const std::string& word, const std::set<std::string>& dict) {
+    return dict.count(word);
 }
 
 // Define any helper functions here
+void getWords(const std::string& input, const std::string& floating, size_t index,std::string current,std::set<std::string>& words){   
+
+    //case 1
+    if(current.size() == input.size()){ 
+      words.insert(current); 
+      return;
+    }
+    //case 2
+		
+    else if(input[current.size()] == '-'){
+			std::string str;
+      if(index != floating.size()){
+        for(char character = 'a'; character <= 'z'; character++){
+        	str = floating;
+          if(str.find(character) != std::string::npos) str.erase(str.find(character), 1);
+          getWords(input, str, index - 1, current + character, words);
+        }
+    	}
+      else{
+				for (const auto& character : floating) {
+          str = floating;
+          str.erase(str.find(character), 1);
+          getWords(input, str, index - 1, current + character, words);
+        }
+        
+      }
+    }
+		//case 3
+    else{
+			 current += input[current.size()];
+        getWords(input, floating, index, current, words);   
+    }
+}
+
+
+
